a95d98437e6f679621b77a202feb8dbc906a05f6
Luminary <chamb343@gmail.com>
2024-04-17 12:32:03 -0600

    requests for geolocation and weather added

---
+++ build.rs
@@ -0,0 +1,3 @@
+fn main() {
+    println!("cargo:rustc-env=OPENWEATHER_API_KEY=a8485e56cd60e60301fa4b814f9deb23");
+}
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,20 @@
+use std::env;
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Deserialize)]
 struct ZipStyle {
     zip: String,
     style: String,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+struct GeoLocation {
+    zip: String,
+    name: String,
+    lat: f32,
+    lon: f32,
+    country: String,
 }
 
 #[get("/")]
@@ -17,7 +27,33 @@ async fn hello() -> impl Responder {
 **/
 #[get("/api/zip")]
 async fn clothing_style(info: web::Query<ZipStyle>) -> impl Responder {
+    let key = "OPENWEATHER_API_KEY";
     println!("Country Zip: {}", info.zip);
+    if let Ok(val) = env::var(key) {
+        let response = reqwest::get(format!("http://api.openweathermap.org//geo/1.0/zip?zip={}&appid={}", info.zip, val)).await;
+        let body = match response {
+            Ok(body) => body,
+            Err(_) => return HttpResponse::InternalServerError().body("API request failed")
+        };
+        let body: GeoLocation = serde_json::from_str(&body.text().await.unwrap()).unwrap();
+        println!("Latidude: {}, Longitude: {}", body.lat, body.lon);
+        let response = reqwest::get(format!("http://api.openweathermap.org/data/2.5/onecall?lat={}&lon={}&appid={}", body.lat, body.lon, val)).await;
+        let body = match response {
+            Ok(body) => body,
+            Err(_) => return HttpResponse::InternalServerError().body("API request failed")
+        };
+        println!("{}", val);
+        println!("Response: {:?}", body.text().await.unwrap());
+        /*if let Ok(body) = response {
+            let body: GeoLocation = serde_json::from_str(&body.text().await.unwrap()).unwrap();
+            
+        } else {
+            HttpResponse::InternalServerError().body("API request failed");
+            panic!("API request failed {:?}", response);
+        }*/
+    } else {
+        return HttpResponse::InternalServerError().body("API Key not found");
+    }
     return HttpResponse::Ok().body(format!("The style is: {}", info.style))
 }
 
